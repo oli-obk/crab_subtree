78edc5e553c35ca3bf8ca7ad11c13a8f7a5b640a
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-09-23 15:22:16 +0000

    Rewrite commit walking as iterator

--- src/processor.rs
+++ src/processor.rs
@@ -13,43 +13,43 @@ pub fn process(repo: &Repository, subtree_root_commit: Oid, prefix: PathBuf) -> 
     let commits = walker.collect::<Result<Vec<_>, _>>()?;
 
     // Collect all commits that touch the subtree directory.
-    let mut relevant_commits = vec![];
-
-    'commits: for (i, &oid) in commits.iter().enumerate() {
-        eprint!(
-            "\r{i}/{} {}% found {} relevant commits",
-            commits.len(),
-            i * 100 / commits.len(),
-            relevant_commits.len()
-        );
-        let commit = repo.find_commit(oid)?;
-        if commit.parent_count() > 1 {
-            // We ignore merge commits, as the rustc repo only has trivial merges.
-            trace!(?commit, "skipping merge commit");
-            continue;
-        }
-        trace!(?commit, "found commit in range");
-        let parent;
-        let diff = repo.diff_tree_to_tree(
-            if commit.parent_count() == 1 {
-                parent = commit.parent(0)?.tree()?;
-                Some(&parent)
-            } else {
-                None
-            },
-            Some(&commit.tree()?),
-            None,
-        )?;
-        for delta in diff.deltas() {
-            for file in [delta.old_file(), delta.new_file()] {
-                let path = file.path().unwrap();
-                if path.starts_with(&prefix) {
-                    relevant_commits.push(commit.clone());
-                    continue 'commits;
+    let relevant_commits = commits
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &oid)| {
+            (|| -> Result<_> {
+                eprint!("\r{i}/{} {}%", commits.len(), i * 100 / commits.len());
+                let commit = repo.find_commit(oid)?;
+                if commit.parent_count() > 1 {
+                    // We ignore merge commits, as the rustc repo only has trivial merges.
+                    trace!(?commit, "skipping merge commit");
+                    return Ok(None);
                 }
-            }
-        }
-    }
+                trace!(?commit, "found commit in range");
+                let parent;
+                let diff = repo.diff_tree_to_tree(
+                    if commit.parent_count() == 1 {
+                        parent = commit.parent(0)?.tree()?;
+                        Some(&parent)
+                    } else {
+                        None
+                    },
+                    Some(&commit.tree()?),
+                    None,
+                )?;
+                for delta in diff.deltas() {
+                    for file in [delta.old_file(), delta.new_file()] {
+                        let path = file.path().unwrap();
+                        if path.starts_with(&prefix) {
+                            return Ok(Some(commit));
+                        }
+                    }
+                }
+                Ok(None)
+            })()
+            .transpose()
+        })
+        .collect::<Result<Vec<_>>>()?;
     eprintln!("\rdone                                                   ");
     debug!("{:#?}", relevant_commits);
 
